e927b2ba6e6e3de3a80c82a10f5edbf32a77d0ed
Fraise <n0valyfe@hotmail.fr>
2016-06-30 14:04:14 +0200

    Added getopt, default port and error code.

--- main.c
+++ main.c
@@ -7,9 +7,11 @@
 #include <openssl/err.h>
 
 #define SERIAL "0123456789ABCDEF"
-#define PORT 12012
+#define DEFAULT_PORT 12012
 
-int main(void)
+#define ERROR -1
+
+int main(int argc, char* argv[])
 {
 	SSL_library_init();			/* load encryption & hash algorithms for SSL */                
 	SSL_load_error_strings();		/* load the error strings for good error reporting */
@@ -19,25 +21,50 @@ int main(void)
 	SSL_CTX* ctx = SSL_CTX_new(server_method);
 	SSL* ssl;
 	struct sockaddr_in server_sockaddr;
-	int server_socket, client;
+	int server_socket, client, opt, port = 0;
 	uint sockaddr_len = sizeof(server_sockaddr);
+
+	if ((opt = getopt(argc, argv, "p:")) != -1)
+	{
+		switch (opt)
+		{
+			case 'p':
+				port = atoi(optarg);
+				break;
+			case '?':
+				if (optopt == 'p')
+				{
+					printf("No port specified, using port %d.\n", DEFAULT_PORT);
+					port = DEFAULT_PORT;
+				}
+				break;
+			default:
+				break;
+		}
+	}
+
+	if (port == 0)
+	{
+		printf("No port specified, using port %d.\n", DEFAULT_PORT);
+		port = DEFAULT_PORT;
+	}
 
 	if (SSL_CTX_use_certificate_file(ctx, "myCert.pem", SSL_FILETYPE_PEM) <= 0)
 	{
 		fprintf(stderr, "Failed to load certificate.\n");
-		return -1;
+		return ERROR;
 	}
 
 	if (SSL_CTX_use_PrivateKey_file(ctx, "myKey.pem", SSL_FILETYPE_PEM) <= 0)
 	{
 		fprintf(stderr, "Failed to load certificate.\n");
-		return -1;
+		return ERROR;
 	}
 
 	if (!SSL_CTX_check_private_key(ctx))
 	{
 		fprintf(stderr, "Private key does not match the public certificate.\n");
-		return -1;
+		return ERROR;
 	}
 
 	ssl = SSL_new(ctx);
@@ -47,25 +74,27 @@ int main(void)
 	if (server_socket < 0)
 	{
 		fprintf(stderr, "Failed to create socket.\n");
-		return -1;
+		return ERROR;
 	}
 	 
 	memset(&server_sockaddr, 0, sizeof(server_sockaddr));
 	server_sockaddr.sin_family = AF_INET;
 	server_sockaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	server_sockaddr.sin_port = htons(PORT);
+	server_sockaddr.sin_port = htons(port);
 	 
 	if (bind(server_socket, (struct sockaddr*)&server_sockaddr,sizeof(server_sockaddr)) < 0)
 	{
 		fprintf(stderr, "Failed to bind socket.\n");
-		return -1;
+		return ERROR;
 	}
-	 
+
+	printf("Socket binded\n");
+
 	/* Receive a TCP connection. */
 	if (listen(server_socket, 5) < 0)
 	{
 		fprintf(stderr, "Failed to listen to TCP connection.\n");
-		return -1;
+		return ERROR;
 	}
 
 	client = accept(server_socket, (struct sockaddr*)&server_sockaddr, &sockaddr_len);
@@ -73,7 +102,7 @@ int main(void)
 	if (client < 0)
 	{
 		fprintf(stderr, "Failed to accept TCP connection.\n");
-		return -1;
+		return ERROR;
 	}
 
 	SSL_set_fd(ssl, client);
@@ -81,7 +110,7 @@ int main(void)
 	if (SSL_accept(ssl) <= 0)
 	{
 		fprintf(stderr, "Failed to initiate SSL handshake.\n");
-		return -1;
+		return ERROR;
 	}
 	else
 	{
